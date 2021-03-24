3cdd0a85cbcc3e44737b8d1b6ce7c2f90d97f212
esma17 <milkysf@gmail.com>
2021-03-24 11:32:03 -0500

    regression

--- src/test/java/com/libraryAutomation/runners/CukesRunner.java
+++ src/test/java/com/libraryAutomation/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/libraryAutomation/stepDefinitions",
         dryRun = false,
-        tags = "@regression or @smoke"
+        tags = "@regression or @smoke not @bookcat"
 )
 
 
