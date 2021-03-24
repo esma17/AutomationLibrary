1f4278b8055d8bceda886d0e4f471adda5d17b70
esma17 <milkysf@gmail.com>
2021-03-24 11:25:09 -0500

    regression

--- src/test/java/com/libraryAutomation/utilities/Driver.java
+++ src/test/java/com/libraryAutomation/utilities/Driver.java
@@ -2,17 +2,19 @@ package com.libraryAutomation.utilities;
 
 import com.libraryAutomation.utilities.enumOptions.Browser;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 public class Driver {
     private static final ThreadLocal<WebDriver> driverPoll = new ThreadLocal<>();
@@ -29,7 +31,7 @@ public class Driver {
                             DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                             desiredCapabilities.setBrowserName(BrowserType.CHROME);
                             desiredCapabilities.setCapability("platform", Platform.WINDOWS);
-                            URL url=new URL("http://34.228.73.245:4444/wd/hub");
+                            URL url = new URL("http://34.228.73.245:4444/wd/hub");
                             driverPoll.set(new RemoteWebDriver(url, desiredCapabilities));
                         } catch (Exception e) {
                             e.printStackTrace();
@@ -55,12 +57,24 @@ public class Driver {
                         WebDriverManager.firefoxdriver().setup();
                         driverPoll.set(new FirefoxDriver());
                         driverPoll.get().manage().window().maximize();
-                       // driverPoll.get().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                        // driverPoll.get().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
                         break;
                     case safari:
                         driverPoll.set(new SafariDriver());
                         driverPoll.get().manage().window().maximize();
                         //driverPoll.get().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                        break;
+                    case headless_chrome:
+                        WebDriverManager.chromedriver().setup();
+                        ChromeOptions options = new ChromeOptions();
+                        options.addArguments("--headless");
+                        driverPoll.set((new ChromeDriver(options)));
+                        driverPoll.get().manage().window().setSize(new Dimension(1200, 900));
+                        break;
+                    case firefox_headless:
+                        WebDriverManager.firefoxdriver().setup();
+                        driverPoll.set(new FirefoxDriver(new FirefoxOptions().setHeadless(true)));
+                        driverPoll.get().manage().window().setSize(new Dimension(1200, 900));
                         break;
                 }
             }
